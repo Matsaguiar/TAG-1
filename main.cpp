b254105fe21c62f67d12eaf6e814c4552d61e74c
Matsaguiar <42449946+Matsaguiar@users.noreply.github.com>
2020-09-09 18:27:55 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "Vertice.h"
 
+using namespace std;
+
 int main(){
+    S_Grafo Valores;
     vector<vector<int>> Grafo;
 
 
 
 
-    Imprimir_grago(Grafo);
+    Imprimir_grafo(Grafo, Valores);
     return 0;
 }
