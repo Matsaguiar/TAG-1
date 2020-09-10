258e663a3e0f76251c11f8d8e0089fd9ea5a0a7d
Matsaguiar <42449946+Matsaguiar@users.noreply.github.com>
2020-09-10 08:57:22 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include "Vertice.h"
 
 int main(){
-    // S_Grafo* Valores;
-    // cout << &Grafo << endl;
-    // cout << &Valores << endl;
-    
-    int Va, Vb, Vc;
-    Obter_tamanho(&Va, &Vb, &Vc);
-    vector<vector<int>> Grafo(Va+1);
 
-    Grafo = Obter_dados(Va, Vb, Vc);
+    int Va, Vb, Vc;                     //Armazena os 3 primeiros valores do arquivo para montar o grafo;
+
+    FILE* fd;                           //Cria o ponteiro do arquivo;
+
+    char file[] = "soc-dolphins.mtx";   //Abre o arquivo soc-dolphins.mtx;
+    fd = fopen(file, "r");
+
+    Obter_tamanho(&Va, &Vb, &Vc, fd);   //Obtem a quantidade de vertices do grafo;
+
+    vector<vector<int>> Grafo(Va+1);    //Cria o grafo;
+
+    Grafo = Obter_dados(Va, Vb, Vc, fd);//Obtem os elos de cada vertice;
     
     Imprimir_grafo(Grafo);
+
     return 0;
 }
