5d4d5ec183b1fc82144752105c1d912ec8d4a89c
Matsaguiar <42449946+Matsaguiar@users.noreply.github.com>
2020-09-13 10:42:54 -0300

    Update CoefAglo.cpp

--- CoefAglo.cpp
+++ CoefAglo.cpp
@@ -1,93 +1,57 @@
-#include <iostream>
-#include <stdio.h>
-#include <vector>
-//#include "CoefAglo.h"
+#include "CoefAglo.h"
 
 using namespace std;
 
+pair<int,double> coefAglomeracao(vector<vector<int>> Grafo, int vert){
+    int possiveisTriang = (Grafo[vert-1].size() * (Grafo[vert-1].size() - 1)) / 2;
+    double coefVertice = 0;
+    double triangulos = 0;
 
-void Obtem_coef_vertice(vector<vector<int>> Grafo, int tamCoef){
+    if(possiveisTriang > 0 && Grafo[vert-1].size() > 1){
+        int a = vert;
     
-    vector<double> coefAglomeracao(tamCoef);
+        for(int i = 0; i < Grafo[a-1].size(); i++){   
+            int b = Grafo[a-1][i];
 
-    double possivelTriang;
-    double coefVertice;
-    double triangulos;
+            for(int j = 0; j < Grafo[b-1].size(); j++){
+                int c = Grafo[b-1][j];
 
-    for(int i = 0; i < 7; i++){
-        cout << "i = " << i << endl;
-        if(Grafo[i].size() > 1){ //Verifica se ha vizinhos para formar possiveis triangulos;            
-            triangulos = 0;
-            possivelTriang = (Grafo[i].size()*(Grafo[i].size()-1))/2;
-
-            for(int j = 0; j < Grafo[i].size()-1; j++){
-                cout << "ENTROU2" << endl;
-                
-                if(Grafo[i].size() > 1 && Grafo[i+1].size() > 1){
-                    int inf = Grafo[i][j+1] - 1;
-
-                    for(int k = 0; k < Grafo[inf].size(); k++){
-                        cout << " Grafo[i][j]= " << Grafo[i][j] << " Grafo[i][j+1] = " << Grafo[i][j+1] << endl;
-                        printf("Inf: %d  - Grafo[%d][%d] =  %d = Grafo[%d][%d] = %d\n", inf, i, j, Grafo[i][j], inf, k, Grafo[inf][k]);    
-                        if(Grafo[i][j] == Grafo[inf][k]){
-                            cout << "ENTROU5" << endl;
-                            //triangulos = (triangulos + 1)/2; 
+                if(c != a){
+                    for(int k = 0; k < Grafo[c-1].size(); k++){                
+                        if(Grafo[c-1][k] == a){
                             triangulos++;
-                        }                
+                            break;
+                        }
                     }
-                }else
-                    cout << "Nao entrou" << endl;
+                }
             }
-            //triangulos = triangulos/3;
-            cout << triangulos << endl << "PosTri " << possivelTriang << endl;;
-            coefVertice = triangulos/possivelTriang;
-            cout << coefVertice << endl;
-            coefAglomeracao[i] = coefVertice;
-            //cout << coefAglomeracao[i] << endl;
         }
-        /* else{
-            coefAglomeracao[i] = 0;
-            cout << "Coef = " << coefAglomeracao[i] << endl;
-        } */
+        coefVertice = triangulos/(Grafo[vert-1].size() * (Grafo[vert-1].size() - 1));
     }
-    
-    cout << "Coeficientes: " << endl;
-    /* for(int i = 0; i < 7; i++){
-        cout << i+1 << " - ";     //Imprime os vertices;                
-        cout << "Coef: " << coefAglomeracao[i]; 
-        cout << endl;
-    } */
+
+    pair<int, double> n = make_pair(vert, coefVertice);
+
+    return n;
 }
 
-int main(){
-    vector<vector<int>> Grafo(7);
+void Obtem_coef_vertice(vector<vector<int>> Grafo){
 
-    FILE* fd;                           //Cria o ponteiro do arquivo;
+    cout << endl << "\tCoeficiente de Aglomeracao dos vertices" << endl << endl;
+    for(int i = 1; i <= Grafo.size(); i++){
 
-    char file[] = "soc.txt";   //Abre o arquivo soc-dolphins.mtx;
-    fd = fopen(file, "r");
+        pair <int, double> n = coefAglomeracao(Grafo, i);
+        printf("Vertice: %d - %.2f\n", n.first, n.second);
+    }
+}
 
-    if(fd == NULL){                     //Verifica se o arquivo abriu com sucesso;
-        cout << "Erro abrir arquivo" << endl;
-        return 0;
+void Obtem_coef_medio(vector<vector<int>> Grafo){
+    double soma = 0.0;
+
+    for(int i = 0; i < Grafo.size(); i++){
+        pair<int, double> p = coefAglomeracao(Grafo, i+1);
+        soma = soma + p.second;
     }
 
-    for(int i = 0; i < 7; i++){    //Faz a leitura dos vertices e seus respectivos elos;
-        int a, b;
-        fscanf(fd, "%d %d", &a, &b);
-        Grafo[a-1].push_back(b);
-        Grafo[b-1].push_back(a);
-    }   
-    /* cout << "Vertices: " << endl;
-    for(int i = 0; i < 7; i++){
-        cout << i+1 << " - ";     //Imprime os vertices;
-        for(int j = 0; j < Grafo[i].size(); j++){
-            printf("%d ", Grafo[i][j]);
-        }                   
-        cout << "Grau: " << Grafo[i].size(); //Imprime o grau de cada vertice;
-        cout << endl;
-    } */
-    Obtem_coef_vertice(Grafo, 7);
-    
-    return 0;
+    printf("Coeficiente Medio de Aglomeracao: %.2f\n", soma/(Grafo.size()));
+
 }
